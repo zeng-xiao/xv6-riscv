38bce78b607903eee1fdb2a22ea43e1f0385469f
zengxiao <zengxiao@eswin.com>
2021-03-08 09:13:25 +0800

    rv64 gef

--- debug.sh
+++ debug.sh
@@ -2,5 +2,5 @@ ps -ef | grep 'qemu-system-riscv64' | grep -v 'grep'| awk '{print $2}'| xargs ki
 sleep 1
 make qemu-gdb > /dev/null 2>&1 &
 sleep 1 
-riscv64-unknown-elf-gdb
+riscv64-unknown-elf-gdb -q |& tee gdb.log
 ps -ef | grep 'qemu-system-riscv64' | grep -v 'grep'| awk '{print $2}'| xargs kill -9 > /dev/null 2>&1 &
